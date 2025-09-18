9893a7465ca9cf8a26c958a72aa442c53ae9bb80
shubham somavamshi <shubhamraopatil123@gmail.com>
2025-09-18 23:21:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+
+const username = document.getElementById("username"); 
+const Password = document.getElementById("password"); 
+const checkbox = document.getElementById("checkbox"); 
+const submit = document.getElementById("submit"); 
+
+
+submit .addEventListener("click",(e)=>{
+  e.preventDefault();
+
+if(checkbox.checked){
+   localStorage.setItem("username",username.value);
+   localStorage.setItem("password",Password.value);
+alert(`Logged in as ${username.value}`);
+}else{
+   localStorage.removeItem("username");
+    localStorage.removeItem("password");
+    alert(`Logged in as ${username.value} (credentials not saved)`);
+}
+
+const savedUsername = localStorage.getItem("username");
+const savedPassword = localStorage.getItem("password");
+
+if(savedUsername && savedPassword){
+  const existingBtn = document.getElementById("existing");
+   existingBtn.hidden = false;
+
+    existingBtn.addEventListener("click", () => {
+  const savedUsername = localStorage.getItem("username");
+  alert(`Logged in as ${savedUsername}`);
+});
+}
+});
