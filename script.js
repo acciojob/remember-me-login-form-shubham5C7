7a600f1d41e68dc19aac347eea9238c0883f6a2a
shubham somavamshi <shubhamraopatil123@gmail.com>
2025-09-19 09:56:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,37 @@
-//your JS code here. If required.
-
 const username = document.getElementById("username"); 
-const Password = document.getElementById("password"); 
+const password = document.getElementById("password"); 
 const checkbox = document.getElementById("checkbox"); 
 const submit = document.getElementById("submit"); 
-
-
-submit .addEventListener("click",(e)=>{
-  e.preventDefault();
-
-if(checkbox.checked){
-   localStorage.setItem("username",username.value);
-   localStorage.setItem("password",Password.value);
-alert(`Logged in as ${username.value}`);
-}else{
-   localStorage.removeItem("username");
-    localStorage.removeItem("password");
-    alert(`Logged in as ${username.value} (credentials not saved)`);
-}
+const existingBtn = document.getElementById("existing");
 
 const savedUsername = localStorage.getItem("username");
 const savedPassword = localStorage.getItem("password");
 
 if(savedUsername && savedPassword){
-  const existingBtn = document.getElementById("existing");
    existingBtn.hidden = false;
+}
 
-    existingBtn.addEventListener("click", () => {
+submit.addEventListener("click",(e)=>{
+  e.preventDefault();
+
+  if(checkbox.checked){
+     localStorage.setItem("username", username.value);
+     localStorage.setItem("password", password.value);
+     existingBtn.hidden = false;
+     alert(`Logged in as ${username.value}`);
+  } else {
+     localStorage.removeItem("username");
+     localStorage.removeItem("password");
+     existingBtn.hidden = true;
+     alert(`Logged in as ${username.value} (credentials not saved)`);
+  }
+});
+
+existingBtn.addEventListener("click", () => {
   const savedUsername = localStorage.getItem("username");
   alert(`Logged in as ${savedUsername}`);
 });
-}
-});
+
+
+
+
