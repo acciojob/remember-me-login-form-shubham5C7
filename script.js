b15345680b9de69cf3237539714cb003eaf2e890
shubham somavamshi <shubhamraopatil123@gmail.com>
2025-09-19 10:16:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,38 @@
-const username = document.getElementById("username"); 
-const password = document.getElementById("password"); 
-const checkbox = document.getElementById("checkbox"); 
-const submit = document.getElementById("submit"); 
-const existingBtn = document.getElementById("existing");
+const username = document.getElementById("username");
+    const password = document.getElementById("password");
+    const checkbox = document.getElementById("checkbox");
+    const submit = document.getElementById("submit");
+    const existingBtn = document.getElementById("existing");
 
-const savedUsername = localStorage.getItem("username");
-const savedPassword = localStorage.getItem("password");
+  
+    const savedUsername = localStorage.getItem("username");
+    const savedPassword = localStorage.getItem("password");
 
-if(savedUsername && savedPassword){
-   existingBtn.hidden = false;
-}
+    if (savedUsername && savedPassword) {
+      existingBtn.hidden = false;
+    }
 
-submit.addEventListener("click",(e)=>{
-  e.preventDefault();
-  if(checkbox.checked){
-     localStorage.setItem("username", username.value);
-     localStorage.setItem("password", password.value);
-     existingBtn.hidden = false;
-     alert(`Logged in as ${username.value}`);
-  } else {
-     localStorage.removeItem("username");
-     localStorage.removeItem("password");
-     existingBtn.hidden = true;
-     alert(`Logged in as ${username.value} (credentials not saved)`);
-  }
-});
+    
+    submit.addEventListener("click", (e) => {
+      e.preventDefault();
+      if (checkbox.checked) {
+        localStorage.setItem("username", username.value);
+        localStorage.setItem("password", password.value);
+        existingBtn.hidden = false;
+      } else {
+        localStorage.removeItem("username");
+        localStorage.removeItem("password");
+        existingBtn.hidden = true;
+      }
+     
+      alert(`Logged in as ${username.value}`);
+    });
 
-existingBtn.addEventListener("click", () => {
-  const savedUsername = localStorage.getItem("username");
-  alert(`Logged in as ${savedUsername}`);
-});
-
+    // existing user button
+    existingBtn.addEventListener("click", () => {
+      const savedUsername = localStorage.getItem("username");
+      alert(`Logged in as ${savedUsername}`);
+    });
 
 
 
